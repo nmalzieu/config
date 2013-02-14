98ccefeb575332fa20bf1ae3aa8e17d27e5d145d
nmalzieu <nmalzieu@work4labs.com>
2013-02-14 12:43:52 -0800

    git prompt better

--- .bash/git_branch_prompt.sh
+++ .bash/git_branch_prompt.sh
@@ -1,12 +1,35 @@
-# http://henrik.nyh.se/2008/12/git-dirty-prompt
-# http://www.simplisticcomplexity.com/2008/03/13/show-your-git-branch-name-in-your-prompt/
-#   username@Machine ~/dev/dir[master]$   # clean working directory
-#   username@Machine ~/dev/dir[master*]$  # dirty working directory
+function find_git_branch {
+   local dir=. head
+   until [ "$dir" -ef / ]; do
+      if [ -f "$dir/.git/HEAD" ]; then
+         head=$(< "$dir/.git/HEAD")
+         if [[ $head == ref:\ refs/heads/* ]]; then
+            git_branch=" (${head#*/*/})"
+         elif [[ $head != '' ]]; then
+            git_branch=' (detached)'
+         else
+            git_branch=' (unknown)'
+         fi
+         return
+      fi
+      dir="../$dir"
+   done
+   git_branch=''
+}
+function find_git_dirty {
+    st=$(git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]; then
+        git_dirty=''
+    elif [[ $st == "nothing to commit (working directory clean)" ]]; then
+        git_dirty=''
+    else
+        git_dirty='*'
+    fi
+}
+PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
 
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
-}
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
-}
-export PS1='\u@\h \[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
+# Default Git enabled prompt with dirty state
+# export PS1="\u@\h \w\[$txtcyn\]\$git_branch\[$txtylw\]\$git_dirty\[$txtrst\]\$ "
+
+# Default Git enabled root prompt (for use with "sudo -s")
+# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ 
