c13fc567ff0ab604fa5a10e4c5a2095ebdc948c9
nmalzieu <nmalzieu@work4labs.com>
2013-02-14 12:41:39 -0800

    added two bash files: print in colors and get git branch in prompt

---
+++ .bash/print_colors.sh
@@ -0,0 +1,12 @@
+function red() {
+        printf "\033[1;31m$1\033[0m "
+}
+
+function green() {
+        printf "\033[1;32m$1\033[0m "
+}
+
+function blue() {
+        printf "\033[1;34m$1\033[0m "
+}
+
