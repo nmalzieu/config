3645511bd0619fef81808ce83636f668383a1020
nmalzieu <nmalzieu@work4labs.com>
2013-02-14 14:46:48 -0800

    exporting term xterm not screen

--- .bash/shared_bash.sh
+++ .bash/shared_bash.sh
@@ -2,4 +2,4 @@ source ~/.bash/colors.sh
 source ~/.bash/django_bash_completion.sh
 source ~/.bash/print_colors.sh
 source ~/.bash/git_branch_prompt.sh
-export TERM=screen-256color
+export TERM=xterm-256color
