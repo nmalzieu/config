330acd2586e2367c9395822c02ad5f738ce551df
nmalzieu <noe.malzieu@gmail.com>
2016-11-30 11:29:10 +0100

    removed shared_functions and using vim as VISUAL

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,12 @@ set -gx OMF_PATH "$HOME/.local/share/omf"
 
 set -gx LSCOLORS Gxfxcxdxbxegedabagacad
 
+set -gx VISUAL vim
+
 # Loading functions
 
 set fish_config_path $HOME/.config/fish
 . $fish_config_path/functions.fish
-. $fish_config_path/shared_functions.fish
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
--- .config/fish/shared_functions.fish
+++
@@ -1,3 +0,0 @@
-function dnsflush
-    sudo discoveryutil udnsflushcaches
-end
