55f71e19cc02290d38d56838d0ca678da704f819
nmalzieu <noe.malzieu@gmail.com>
2022-01-07 16:08:42 +0100

    Cleanup

--- .bash/screen_ssh_agent.sh
+++
@@ -1,8 +0,0 @@
-# Predictable SSH authentication socket location.
-SOCK="/tmp/ssh-agent-$USER-screen"
-if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
-then
-    rm -f /tmp/ssh-agent-$USER-screen
-    ln -sf $SSH_AUTH_SOCK $SOCK
-    export SSH_AUTH_SOCK=$SOCK
-fi
--- .bash/shared_bash.sh
+++
@@ -1,5 +0,0 @@
-source ~/.bash/colors.sh
-source ~/.bash/git-completion.sh
-source ~/.bash/django_bash_completion.sh
-source ~/.bash/print_colors.sh
-source ~/.bash/git_branch_prompt.sh
